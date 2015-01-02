e082e508b3bfdd4e6aef36fd281b1440fc13d6d6
Johan Kardell <johan.kardell@gmail.com>
2015-01-02 11:59:39 +0100

    Cleaned up

--- app/javascripts/statusUI.js
+++ app/javascripts/statusUI.js
@@ -17,23 +17,3 @@ function StatusUI(nextPlayerElement, winnerElement){
     _winnerElement.textContent = '';  
   };
 }
-
-/*
-function StatusUI(nextPlayerElement, winnerElement){
-  this.nextPlayerElement = nextPlayerElement;
-  this.winnerElement = winnerElement;
-}
-
-StatusUI.prototype.nextPlayer = function(player){
-  this.nextPlayerElement.textContent = 'Next: ' + player;
-};
-
-StatusUI.prototype.winnerIs = function(winner){
-  this.winnerElement.textContent = 'Winner is: ' + winner;
-};
-
-StatusUI.prototype.clear = function(){
-  this.nextPlayerElement.textContent = '';
-  this.winnerElement.textContent = '';  
-};
-*/
